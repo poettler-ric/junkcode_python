ecccba48403fe5b84d398b12dd698b1f2c7e9e95
Richard Poettler <richard.poettler@gmail.com>
2010-10-04 12:01:44 +0200

    set ilf logins to something useful

---
+++ sqlalchemy/ilf/set_employee_logins.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""
+Updates a table with values taken from another joined table.
+"""
+
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.sql import select, update
+from sqlalchemy.sql.expression import func
+
+user = "inetusr"
+password = "inetpw"
+host = "localhost"
+port = 3306
+db = "inet"
+
+# setup engine and bound metadata
+engine = create_engine("mysql+mysqlconnector://%s:%s@%s:%d/%s"
+        % (user, password, host, port, db))
+metadata = MetaData(engine)
+
+# reflect the needed tables
+persons = Table("p_Person", metadata, autoload=True)
+employee = Table("e_Employee", metadata, autoload=True)
+employee_data = Table("ed_EmployeeData", metadata, autoload=True)
+location = Table("loc_Location", metadata, autoload=True)
+employee_training = Table("et_EmployeeTraining", metadata, autoload=True)
+employee_training_status = Table("value_EmployeeTrainingStatus", metadata, autoload=True)
+
+# prepare the query from which to select the values
+query = select([func.concat(persons.c.P_Name, ", ", persons.c.P_FirstNames)],
+    persons.c.P_ID==employee.c.E_P_ID).limit(1)
+
+# create a update which takes the values from the query
+update = employee.update().values(E_Login=query)
+
+print str(update)
+update.execute()
