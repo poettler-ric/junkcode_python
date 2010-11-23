e59ae20543c5cd375497da9956c8efa81d916983
Richard Poettler <richard.poettler@gmail.com>
2010-11-23 11:15:01 +0100

    first version of a ajax chat application

---
+++ cherrypy/chat/server.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import cherrypy
+import json
+import os.path
+
+class Controller(object):
+    def __init__(self):
+        self.messages = []
+
+    @cherrypy.expose()
+    @cherrypy.tools.redirect(url='/static/chat.html', internal=False)
+    def index(self):
+        return "you shouldn't see this"
+
+    @cherrypy.expose()
+    def postMessage(self, user, message, lastId=-1):
+        lastId = int(lastId)
+        self.messages.append({'user': user, 'message': message})
+        return json.dumps(self.messages[lastId+1:])
+
+    @cherrypy.expose()
+    def getMessage(self, lastId=-1):
+        lastId = int(lastId)
+        return json.dumps(self.messages[lastId+1:])
+
+    @cherrypy.expose()
+    def clear(self):
+        self.messages = []
+        return json.dumps(self.messages)
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+config = {
+    '/static': {
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': os.path.join(current_dir, 'static'),
+    }
+}
+
+cherrypy.tree.mount(Controller(), '/', config)
+cherrypy.engine.start()
+cherrypy.engine.block()
---
+++ cherrypy/chat/static/chat.js
@@ -0,0 +1,38 @@
+var lastId = -1
+
+function readMessages(messages) {
+    output = $('#messages');
+    if (messages.length > 0) {
+	$.each(messages, function(index, message) {
+		output.append(message.user + ': ' + message.message + '\n');
+		lastId++;
+	});
+    }
+    output.scrollTop(99999); // hope we don't exceed 99999 lines...
+}
+
+function sendMessage() {
+    user = $.trim($('#user').val());
+    message = $.trim($('#message').val());
+    data = {
+	lastId: lastId,
+	user: user,
+	message: message,
+    }
+    $.getJSON('/postMessage', data, readMessages);
+ }
+
+function retrieveMessages() {
+    $.getJSON('/getMessage', { lastId: lastId }, readMessages);
+    setTimeout('retrieveMessages();', 1000);
+}
+
+$(document).ready(function() {
+    retrieveMessages();
+    $('#message').keydown(function(e) {
+	if (e.keyCode == 13) {
+	    sendMessage();
+	    $('#message').val("");
+	}
+    })
+})
