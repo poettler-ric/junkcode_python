6dfe9482345209dafd2ce445bcc5977024d6c1cb
Richard Poettler <richard.poettler@gmail.com>
2011-01-25 09:14:27 +0100

    finished pythonchallenge 6

--- pythonchallenge/challenge6.py
+++ pythonchallenge/challenge6.py
@@ -2,49 +2,37 @@
 
 """
 http://www.pythonchallenge.com/pc/def/channel.html
+
+Challenge 6 shows a image of a zip and some hints in the html source.
 """
 
 import os
-import os.path
-import shutil
 import tempfile
 import urllib
 import zipfile
 import re
 
 url = "http://www.pythonchallenge.com/pc/def/channel.zip"
-file_name = os.path.basename(url)
-#file_name = tempfile.mktemp('-challenge6')
-
-directory_name = tempfile.mkdtemp('-challenge6')
-#os.mkdir(directory_name)
-
+file_name = tempfile.mktemp('-challenge6')
 next_file = '90052.txt' # taken from readme.txt in the zip
+next_pattern = re.compile(r'^Next nothing is (\d+)$')
 
-next_pattern = re.compile(r'Next nothing is (\d+)')
+urllib.urlretrieve(url, file_name)
 
-#urllib.urlretrieve(url, file_name)
+comment = None
+collected = ""
 
-files = None
-
-with open(file_name) as file:
-    zip = zipfile.ZipFile(file)
-    files = set(zip.namelist())
-    zip.extractall(directory_name)
-
-while next_file:
-    files.remove(next_file)
-    with open(os.path.join(directory_name, next_file)) as file:
-        content = file.read()
-        print content
-
+with zipfile.ZipFile(file_name, 'r') as zip:
+    while next_file:
+        content = zip.read(next_file)
         match = next_pattern.match(content)
+        collected += zip.getinfo(next_file).comment
         if match:
             next_file = match.group(1) + '.txt'
         else:
+            print content
             next_file = None
 
-print files
+print collected
 
-#os.remove(file_name)
-shutil.rmtree(directory_name)
+os.remove(file_name)
