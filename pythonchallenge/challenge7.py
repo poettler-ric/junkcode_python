efe57b5cc8ec53c0af5e0bd86c646fe7dc7c90bf
Richard Poettler <richard.poettler@gmail.com>
2011-01-25 11:01:59 +0100

    determine the grey area programmically

--- pythonchallenge/challenge7.py
+++ pythonchallenge/challenge7.py
@@ -16,26 +16,36 @@ file_name = tempfile.mktemp('-challenge7')
 
 urllib.urlretrieve(url, file_name)
 
-box_coordinates = (0, 43, 608, 52) # left, upper, right, lower pixels
 image = Image.open(file_name)
-box_pixels = image.crop(box_coordinates).load()
+pixels = image.load()
 
-last_value = ord(' ') # initialize with a space
+# determine the fist grey pixel in the first row
+begin_y = -1
+for i in xrange(image.size[1]):
+    pixel = pixels[0, i]
+    if pixel[0] == pixel[1] == pixel[2]:
+        begin_y = i
+        break
+
+# determine the last nongrey pixel in the grey row
+end_x = -1
+for i in xrange(image.size[0]):
+    pixel = pixels[i, begin_y]
+    if not pixel[0] == pixel[1] == pixel[2]:
+        end_x = i
+        break
+
 max_area_width = 7 # we assume the maximum area width (for double letters)
-count = 0
 collected = ""
 
-for i in xrange(box_coordinates[2]):
-    value = box_pixels[i, 0][0]
-    if value != last_value or count >= max_area_width:
-        collected += chr(last_value)
-        last_value = value
-        count = 1
-    else:
-        count += 1
+# iterate over the grey bar
+for i in xrange(0, end_x, max_area_width):
+    red_value = pixels[i, begin_y][0]
+    collected += chr(red_value)
 
 print collected
 
+# extract the list in the string
 ascii_list = eval(collected[collected.find('[') : collected.find(']') + 1])
 print "the solution is:", ''.join(map(chr, ascii_list))
 
